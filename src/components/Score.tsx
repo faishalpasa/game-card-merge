5c67bf0dfb5c67e13ac7998f64a76304d2235e1b
Faishal Pasa <faishal.pasa@bareksa.com>
2025-03-09 08:03:13 +0700

    feat: encrypt

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -8,9 +8,9 @@ interface ScoreProps {
 export const Score = ({ score, scorePerSecond }: ScoreProps) => {
   return (
     <div className="flex flex-col justify-center items-center absolute top-2.5 left-2.5 right-2.5">
-      <div className="text-3xl text-black">{formatNumber(score)}</div>
+      <div className="text-3xl text-black">{formatNumber(+score)}</div>
       <div className="text-sm text-gray-500">
-        +{formatNumber(scorePerSecond)}/s
+        +{formatNumber(+scorePerSecond)}/s
       </div>
     </div>
   )
